43f1d4b2245529d59ccf212282982b9da9aa60e8
RaulQX <raul.sarbu@student.upt.ro>
2020-08-11 20:04:23 +0200

    Fixed case no comment box

--- main.py
+++ main.py
@@ -99,7 +99,9 @@ class InstagramBot:
         if not h == w == 40:
             like_btn = buttons[3]
 
-        comm_box = self.browser.find_element_by_css_selector("form textarea")
+        comm_box = self.browser.find_elements_by_css_selector("form textarea")
+        if comm_box:
+            comm_box = comm_box[0]
 
         return [follow_btn, like_btn, comm_box]
 
@@ -109,11 +111,17 @@ class InstagramBot:
             # todo: check if post was previously seen
             print(post_url)
             follow_btn, like_btn, comm_box = self.get_post_details(post_url)
+
             if follow_btn.text.lower() == "follow":
                 follow_btn.click()
                 time.sleep(2)
+
             like_btn.click()
             time.sleep(2)
+
+            if not comm_box:
+                time.sleep(2)
+                return
             comm_box.click()
             comm_box = self.browser.find_element_by_css_selector("form textarea")
             comms = random.choice(self.settings["base_comments"])
