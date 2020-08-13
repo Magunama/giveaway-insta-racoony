262742f012ac5582caf63c0877df0755b7db811d
Robert Ciama <robert.ciama00@e-uvt.ro>
2020-08-13 22:29:43 +0300

    Split post actions in methods

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ import sys
 
 opts = Options()
 
+
 def load_settings():
     fchk = FileCheck()
     fchk.check_create_folder("data")
@@ -18,6 +19,7 @@ def load_settings():
     with open("data/settings.json") as f:
         print("[INFO] Loaded settings.")
         return json.load(f)
+
 
 def parse_arguments():      
     """
@@ -31,11 +33,12 @@ def parse_arguments():
     opts.start = 9
 
     for arg in sys.argv:
-
         if arg == "-h":
             opts.add_argument("--headless")
         elif arg == "-hot":
             opts.start = 0
+
+# todo: clear follow list
 
 
 class InstagramBot:
@@ -109,56 +112,56 @@ class InstagramBot:
         old_tab = self.browser.window_handles[0]
         self.browser.switch_to.window(old_tab)
 
-    def get_post_details(self, url):
-        self.browser.get(url)
+    @staticmethod
+    def like(buttons):
+        time.sleep(2)
 
-        buttons = self.browser.find_elements_by_css_selector("button")
-        follow_btn = buttons[0]
         like_btn = buttons[2]
         # h, w = like_btn.size["height"], like_btn.size["width"]
         h, w = like_btn.size.values()
         if not h == w == 40:
             like_btn = buttons[3]
+        like_btn.click()
 
+    @staticmethod
+    def follow(buttons):
+        follow_btn = buttons[0]
+        if follow_btn.text.lower() == "follow":
+            follow_btn.click()
+            time.sleep(2)
+
+    def comment(self):
         comm_box = self.browser.find_elements_by_css_selector("form textarea")
-        if comm_box:
-            comm_box = comm_box[0]
+        if not comm_box:
+            return
 
-        return [follow_btn, like_btn, comm_box]
+        comm_box = comm_box[0]
+        comm_box.click()
+        comm_box = self.browser.find_element_by_css_selector("form textarea")
+
+        # todo: send random number of comments
+        comm_choice = random.choice(self.settings["base_comments"])
+        comm_box.send_keys(comm_choice)
+
+        post_button = self.browser.find_element_by_css_selector("form button")
+        post_button.click()
+
+        self.comm_counter += 1
+        if self.comm_counter == 5:
+            self.comm_counter = 0
+            print("Waiting 5 seconds to avoid soft ban")
+            time.sleep(10)
+
+        time.sleep(2)
 
     def process_posts(self, links):
-
         for i in range(opts.start, len(links)):
             post_url = links[i]
             # todo: check if post was previously seen
             print(post_url)
-            follow_btn, like_btn, comm_box = self.get_post_details(post_url)
 
-            if follow_btn.text.lower() == "follow":
-                follow_btn.click()
-                time.sleep(2)
-
-            like_btn.click()
-            time.sleep(2)
-
-            if not comm_box:
-                time.sleep(2)
-                return
-            comm_box.click()
-            comm_box = self.browser.find_element_by_css_selector("form textarea")
-            comm_choice = random.choice(self.settings["base_comments"])
-
-            post_button = self.browser.find_element_by_css_selector("form button")
-            # todo: send random number of comments
-            comm_box.send_keys(comm_choice)
-            post_button.click()
-
-            self.comm_counter += 1
-
-            if self.comm_counter == 5:
-                self.comm_counter = 0
-                print ("Waiting 10 seconds to avoid soft ban")
-                time.sleep(10)
-
-            time.sleep(2)
-
+            self.browser.get(post_url)
+            buttons = self.browser.find_elements_by_css_selector("button")
+            self.follow(buttons)
+            self.like(buttons)
+            self.comment()
